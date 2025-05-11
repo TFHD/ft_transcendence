a591104c34c7cc5a1e709fae51cd0f6aaacc2025
Mewen Batty <scorpionem86@gmail.com>
2025-05-11 16:45:54 +0200

    cool comments for tournament

---
+++ back/pong/Tournament.js
@@ -0,0 +1,37 @@
+/*
+
+TOURNAMENT
+
+Les joueurs arrivent sur une page, creer tournament et rejoindre tournament
+
+GAME-MASTER------------
+Quand un utilisateur cree un tournament il arrive sur une page avec la liste des joueurs
+dans le tournament, peut etre bouton pour kick/ban du tournament??
+Il y a aussi un bouton pour start le tournament
+-----------------------
+
+CLIENT-----------------
+Quand un utilisateur veux rejoindre un tournament il peut voir une liste des tournaments en attente
+Il peut rejoindre les tournaments et ca le rajoute dans la liste de joueurs du tournament
+-----------------------
+
+Lorsque le game master commence le tournament appuie sur commencer les games se lancent par paire
+et au hasard.
+
+Une fois que les joueurs ont finis leur partie, ils arrivent sur une fenetre avec les resultats
+de la manche du tournoi dans ce style:
+
+
+Player1 ---|
+           |-----Player2 ---|
+Player2 ---|                |
+                            |---Winner
+Player3 ---|                |
+           |-----Player3 ---|
+Player4 ---|
+
+
+Si un joueur est seul, il est considere gagnant
+Si le nombre de paires est impair, le tournoi ne peut pas commencer
+
+*/
