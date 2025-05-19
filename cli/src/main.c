bcca3f49225000d4c41d1c64fa8cf191e68cdeea
Arkturius <63616260+Arkturius@users.noreply.github.com>
2025-05-19 19:10:54 +0200

    one header boi

--- cli/src/main.c
+++ cli/src/main.c
@@ -2,7 +2,6 @@
  *	CLI Transcendence application.
  */
 
-
 #include <transcendence.h>
 
 CURL	*__curl_ctx	= NULL;
@@ -99,157 +98,33 @@ char	__TCLI_tmp[1024]	= {0};
 // 	return (0);
 // }
 
+# include <transcendence.h>
 
-// #include <stdio.h>
-// #include <nopoll.h>
-// #include <curl.h>
-// #include <cJSON.h>
-// 
-// #define	CLI_URL			"https://10.12.1.1:8000/api/"
-// 
-// #define	AUTH_HEADER		"authorization: Bearer "
-// 
-// typedef enum e_cli_reqtype
-// {
-// 	CLI_GET = 1,
-// 	CLI_POST = 2,
-// 	CLI_PATCH = 3,
-// 	CLI_DELETE = 4,
-// }	cli_reqtype;
-// 
-// 
-// int	cli_request_send(cli_reqtype type, const char *endpoint, char **headers, char *post)
-// {
-// 	char				buffer[512] = {0};
-// 	struct curl_slist	*hdr = NULL;
-// 
-// 	strcat(buffer, CLI_URL);
-// 	strcat(buffer, endpoint);
-// 
-// 	while (headers && *headers)
-// 		hdr = curl_slist_append(hdr, *headers++);
-// 
-// 	curl_easy_setopt(CURL_CTX, CURLOPT_HTTPHEADER,		hdr);
-//     curl_easy_setopt(CURL_CTX, CURLOPT_URL,				buffer);
-// 
-// 	if (type == CLI_POST)
-// 		curl_easy_setopt(CURL_CTX, CURLOPT_POSTFIELDS,	post);
-// 
-// 
-//     CURLcode res = curl_easy_perform(CURL_CTX);
-//     if (res != CURLE_OK)
-//         printf("[TRANS-CLI] - Request failed: %s\n", curl_easy_strerror(res));
-// 
-//     curl_slist_free_all(hdr);
-// 	memset(buffer, 0, sizeof(buffer));
-// 	
-// 	curl_easy_reset(CURL_CTX);
-// 	return ((int)res);
-// }
-// 
-// #include <string.h>
-// 
-// struct resp_buf {
-//     char *data;
-//     size_t len;
-// };
-// 
+#define	TCLI_URL_BASE	"https://Trans/api/"
 
-// 
-// int main2(void)
-// {
-// 	int		err = 0;
-// 	char	buffer[1024] = {0};
-// 	char	*hdr[] = {buffer, NULL};
-// 
-// 	CURL_CTX = curl_easy_init();
-//     if(!CURL_CTX)
-// 	{
-//         printf("[TRANS-CLI] - Curl init failed\n");
-// 		goto defer;
-// 	}
-// 
-// 
+#define	AUTH_HEADER		"authorization: Bearer "
 
-
-// 	cli_request_send(CLI_GET, "users/@me", hdr, NULL);
-// 
-// 	cli_request_send(CLI_GET, "auth/2fa/setup", NULL, NULL);
-// defer:
-//     curl_easy_cleanup(CURL_CTX);
-// 	return (err);
-// }
-
-void	TCLI_FUNC(changeScene)(TCLI_SceneCtx *ctx)
+typedef enum e_cli_reqtype
 {
-	TCLI_Elem	*current = ctx->select;
-	TCLI_Scene	new = current->slink;
+	CLI_GET = 1,
+	CLI_POST = 2,
+	CLI_PATCH = 3,
+	CLI_DELETE = 4,
+}	cli_reqtype;
 
-	if (new)
-	{
-		TCLI_SCENE = new;
-		TCLI_STATUS |= TCLI_SCENE_SWAP;
-	}
-}
+struct resp_buf {
+    char *data;
+    size_t len;
+};
 
-void	TCLI_FUNC(handleJump)(TCLI_SceneCtx *ctx, char key)
-{
-	TCLI_Elem	*current = ctx->select;
-	TCLI_Elem	*next = current->nexts[(int)key];
-
-	if (!next)
-		return ;
-
-	current->onDeselect(&current->h);
-
-	if (next == (void *)TCLI_ELEM_LAST)
-	{
-		if (!ctx->last)
-			return ;
-		ctx->select = ctx->last;
-		next = ctx->select;
-	}
-	else
-		ctx->select = next;
-	ctx->last = current;
-
-	next->onSelect(&next->h);
-}
-
-void	TCLI_FUNC(handleInput)(TCLI_SceneCtx *ctx, char key)
-{
-	(void) ctx;
-	(void) key;
-
-	TCLI_Elem	*current = ctx->select;
-
-	if (current->h.type == TCLI_ELEM_TEXTBOX)
-	{
-		uint32_t	len = strlen(current->input);
-		if (len >= 15)
-			return ;
-		current->input[len] = key;
-	}
-}
-
-void	TCLI_FUNC(handleKey)(TCLI_SceneCtx *ctx, char key)
-{
-	if (key < 4)
-		TCLI_handleJump(ctx, key);
-	else if (key == '\n')
-		TCLI_changeScene(ctx);
-	else
-		TCLI_handleInput(ctx, key);
-}
-
-void	TCLI_FUNC(render)(TCLI_SceneCtx *);
+void	TCLI(render)(TCLI_SceneCtx *);
 
 # define	UP_ARROW_SCAN		0x415b1b
 # define	DOWN_ARROW_SCAN		0x425b1b
 # define	RIGHT_ARROW_SCAN	0x435b1b
 # define	LEFT_ARROW_SCAN		0x445b1b
 
-void	TCLI_FUNC(loop)(void)
+void	TCLI(loop)(void)
 {
 	static TCLI_buff	buff_cb = (TCLI_buff)
 	{
@@ -260,7 +135,7 @@ void	TCLI_FUNC(loop)(void)
     curl_easy_setopt(CURL_CTX, CURLOPT_WRITEDATA, &buff_cb);
 
 	TCLI_SceneCtx	*ctx	= NULL;
-	TCLI_SCENE				= &TCLI_debugPage;
+	TCLI_SCENE				= &TCLI_mainMenu;
 	TCLI_STATUS				|= TCLI_SCENE_SWAP;
 
 	printf("width = %u, height = %u\n", TCLI_WIDTH, TCLI_HEIGHT);
@@ -271,8 +146,8 @@ void	TCLI_FUNC(loop)(void)
 			ctx = TCLI_SCENE();
 		if (TCLI_STATUS & TCLI_SCENE_SWAP)
 		{
-			screen_clear(&TCLI_SCREEN);
-			TCLI_render(ctx);
+			TCLI(screenClear)(&TCLI_SCREEN);
+			TCLI(render)(ctx);
 			TCLI_STATUS &= ~TCLI_SCENE_SWAP;
 		}
 
@@ -302,10 +177,12 @@ void	TCLI_FUNC(loop)(void)
 			if (chars == LEFT_ARROW_SCAN)
 				uchar[0] = TCLI_ARROW_LEFT;
 		}
+		if (uchar[0] == 9)
+			uchar[0] = TCLI_ARROW_DOWN;
 
 #if 1
-		TCLI_handleKey(ctx, uchar[0]);
-		TCLI_render(ctx);
+		TCLI(handleKey)(ctx, uchar[0]);
+		TCLI(render)(ctx);
 #endif
 
 //  		TCLI_makeRequest(TCLI_POST | TCLI_REQ_LOGIN);
